295e3449f961cdfcda7fff84ec94990dfa71feeb
Saurabh Totey <SaurabhTotey@gmail.com>
2020-10-20 18:39:05 -0600

    Fixed issues with query parameters

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const toValidDateOfBirth = param => {
-	if (!param || param.length !== 10 || (param[2] != param[5] || param[2] != "/")) {
+	if (!param || param.length !== 10 || (param[2] != param[5] || param[2] != "-")) {
 		return null;
 	}
 	const day = parseInt(param.substring(0, 2));
 	const month = parseInt(param.substring(3, 5));
 	const year = parseInt(param.substring(6, 10));
 	const returnValue = [day, month, year];
-	if (returnValue.some(value => isNan(value))) {
+	if (returnValue.some(value => isNaN(value))) {
 		return null;
 	}
 	return returnValue;
@@ -16,7 +16,7 @@ const urlParameters = new URLSearchParams(window.location.search);
 const dateOfBirth = toValidDateOfBirth(urlParameters.get("dateOfBirth")) || [5, 12, 2000];
 const lifeExpectancy = parseInt(urlParameters.get("lifeExpectancy")) || 90;
 
-const resizeFunction = () => {
+const main = () => {
 	const canvas = document.getElementById("canvas");
 	const renderer = canvas.getContext("2d");
 
@@ -24,10 +24,10 @@ const resizeFunction = () => {
 	canvas.height = document.documentElement.clientHeight;
 
 	const birthDate = new Date();
-	birthDate.setYear(dateOfBirth[2]);
-	birthDate.setMonth(dateOfBirth[1], dateOfBirth[0]);
+	birthDate.setFullYear(dateOfBirth[2]);
+	birthDate.setMonth(dateOfBirth[1] - 1, dateOfBirth[0]);
 	const deathDate = new Date(birthDate.getTime());
-	deathDate.setYear(dateOfBirth[2] + lifeExpectancy);
+	deathDate.setFullYear(dateOfBirth[2] + lifeExpectancy);
 	const today = new Date();
 
 	const msToWeeks = ms => ms / (1000 * 3600 * 24 * 7);
@@ -37,6 +37,8 @@ const resizeFunction = () => {
 	renderer.clearRect(0, 0, canvas.width, canvas.height);
 	const squareSize = Math.min(canvas.width / 52, canvas.height / Math.ceil(weeksTotal / 52));
 	const padding = squareSize / 10;
+
+	// TODO: center drawing on canvas
 
 	let weeksDrawn = 0;
 	let weeksPassedToStillDraw = weeksPassed;
@@ -61,8 +63,8 @@ const resizeFunction = () => {
 		weeksDrawn += squaresToDraw;
 	}
 };
-window.onresize = resizeFunction;
+window.onresize = main;
 
-window.onload = resizeFunction;
+window.onload = main;
 
 //TODO: maybe add some sort of timer to call resizeFunction at the start of each new day
